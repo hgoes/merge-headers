a2522c27016b1995253e7b6abb2b6e976d31f628
Henning Günther <h.guenther@tu-bs.de>
2011-02-16 09:32:31 +0100

    Extract definitions from first header, include pragma once

--- Main.hs
+++ Main.hs
@@ -6,9 +6,11 @@ import Data.Either
 import System.Environment
 import Language.C
 import Language.C.System.GCC
+import Language.C.System.Preprocess
 import Data.List (sortBy)
 import Data.Ord
 import Data.Either
+import qualified Data.ByteString as BS
 
 mergeHeaders :: [CTranslUnit] -> CTranslUnit
 mergeHeaders units = let (mps,rests) = unzip [ generateTypeMap decl | CTranslUnit decl _ <- units ]
@@ -42,5 +44,11 @@ classifyArgument arg
 main = do
   args <- getArgs
   let (files,cpp_args) = partitionEithers $ fmap classifyArgument args
-  cunits <- mapM (getC cpp_args) files
-  print $ pretty $ mergeHeaders cunits
+  pre_res <- runPreprocessor (newGCC "gcc") (rawCppArgs ("-dM":cpp_args) (head files))
+  case pre_res of
+    Left _ -> error "Preprocessing failed"
+    Right defs -> do
+      cunits <- mapM (getC cpp_args) files
+      putStrLn "#pragma once"
+      BS.putStrLn defs
+      print $ pretty $ mergeHeaders cunits
